9064472d75b91a77c37f02fa864ee33afd9e5b1d
Chicca <accara@usi.ch>
2014-12-15 11:30:46 +0100

    Timeout/Username

---
+++ Timeout:User/routes/root/router.js
@@ -0,0 +1,19 @@
+/** @module users/router */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var middleware =  require('../middleware');
+var rootUrl = require("../../config").url;
+
+
+//supported methods
+router.all('/', middleware.supportedMethods('GET, OPTIONS'));
+
+//list users
+router.get('/', middleware.authorize,function(req, res, next) {
+    res.render('library');
+});
+
+/** router for /users */
+module.exports = router;
